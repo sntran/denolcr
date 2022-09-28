2eb64f2ecaa623edb10d7c639ea7175644b6a4c4
Son Tran-Nguyen <github@sntran.com>
2022-09-27 21:45:50 -0600

    feat: static `Rclone.from`

--- README.md
+++ README.md
@@ -11,15 +11,12 @@ const rclone = new Rclone();
 rclone.rc("core/version");
 ```
 
-By default, the provided [`rclone.wasm`](rclone.wasm) module is used. This can be
-changed by providing another compiled module in the constructor. For example:
+By default, the provided [`rclone.wasm`](rclone.wasm) module is used. This can
+be changed by providing another compiled module in the constructor. For example:
 
 ```ts
 import { Rclone } from "https://raw.githubusercontent.com/sntran/denolcr/main/rclone.ts";
-
-const module = await WebAssembly.compileStreaming(fetch("https://sntran.github.io/denolcr/rclone.wasm"));
-const rclone = new Rclone(module);
-
+const rclone = Rclone.from("https://sntran.github.io/denolcr/rclone.wasm");
 rclone.rc("core/version");
 ```
 
--- rclone.ts
+++ rclone.ts
@@ -1,26 +1,32 @@
 // Copyright 2018-2021 Trần Nguyễn Sơn. All rights reserved. MIT license.
 import "https://raw.githubusercontent.com/rclone/rclone/master/fs/rc/js/wasm_exec.js";
 
+function compile(url: string): Promise<WebAssembly.Module> {
+  url = new URL(url, import.meta.url).href;
+  return WebAssembly.compileStreaming(fetch(url));
+}
+
 /** Provides a default WASM module. */
-const wasm = await WebAssembly.compileStreaming(
-  fetch(new URL("./rclone.wasm", import.meta.url))
-);
+const wasm = await compile("./rclone.wasm");
 
 /** A Rclone instance from a compiled WebAssemble module. */
 export class Rclone extends WebAssembly.Instance {
+  static async from(url: string): Promise<Rclone> {
+    const module = await compile(url);
+    return new Rclone(module);
+  }
+
   /**
    * Create a rclone instance.
    */
   constructor(module: WebAssembly.Module = wasm) {
-    // Patches for rclone.
-    // @ts-ignore
+    // @ts-ignore: Patches for rclone.
     globalThis.document ??= {};
-    // @ts-ignore
-    globalThis.rcValidResolve ??= function() {
+    // @ts-ignore: Patches for rclone.
+    globalThis.rcValidResolve ??= function () {
       // Invoked by rclone at the end of initialization.
-    }
-    // Instantiates WASM module.
-    // @ts-ignore
+    };
+    // @ts-ignore: Instantiates WASM module.
     const go = new globalThis.Go(); // From `wasm_exec.js`
 
     super(module, go.importObject);
@@ -28,7 +34,8 @@ export class Rclone extends WebAssembly.Instance {
     go.run(this);
   }
 
-  /** Remote controls rclone
+  /**
+   * Remote controls rclone
    *
    * ```ts
    * import { Rclone } from "https://deno.land/x/rclone@v0.0.2/mod.ts";
@@ -39,8 +46,11 @@ export class Rclone extends WebAssembly.Instance {
    * console.log("operations/list", rc("operations/list", {"fs":":memory:","remote":"bucket"}))
    * ```
    */
-  rc(command: string, args: object): object {
-    // @ts-ignore
+  rc(
+    command: string,
+    args: Record<string, unknown> | null = null,
+  ): Record<string, unknown> {
+    // @ts-ignore: Use global `rc` function.
     return globalThis.rc(command, args);
   }
 }
