5efebdd2fdb59cfe2099c76b99abed91b14b4bda
Sơn Trần-Nguyễn <github@sntran.com>
2024-04-28 14:05:25 -0500

    feat: expose rclone commands
    Rclone WASM only provides `rc`, so we add actual commands on top.

--- rclone.js
+++ rclone.js
@@ -42,6 +42,8 @@ const wasm = await WebAssembly.compileStreaming(
   fetch(new URL("./rclone.wasm", import.meta.url)),
 );
 
+const CONNECTION_STRING = /(.+:)([^:]*)$/;
+
 /** A Rclone instance from a compiled WebAssemble module. */
 export class Rclone extends WebAssembly.Instance {
   /**
@@ -60,6 +62,227 @@ export class Rclone extends WebAssembly.Instance {
     super(module, go.importObject);
 
     go.run(this);
+  }
+
+  /**
+   * Returns the space used on the remote
+   * @param {string} fs a remote name string e.g. "drive:"
+   * @returns {Object} as returned from `rclone about --json`
+   */
+  about(fs) {
+    return this.rc("operations/about", { fs });
+  }
+
+  /**
+   * Runs a backend command
+   * @param {string} command Command name
+   * @param {string} fs a remote name string e.g. "drive:"
+   * @param {Object} args a list of arguments for the backend command
+   * @returns 
+   */
+  backend(command, fs, args = {}) {
+    return this.rc("backend/command", { command, fs, ...args});
+  }
+
+  /**
+   * Checks the files in the source and destination match
+   * @param {string} srcFs a remote name string e.g. "drive:" for the source
+   * @param {string} dstFs a remote name string e.g. "drive2:" for the destination
+   * @param {Object} args arguments similar to ones from `rclone check`
+   * @returns a report of files that don't match
+   */
+  check(srcFs, dstFs, args = {}) {
+    return this.rc("operations/check", { srcFs, dstFs, ...args });
+  }
+
+  /**
+   * Removes trashed files in the remote or path
+   * @param {string} fs 
+   * @returns 
+   */
+  cleanup(fs) {
+    return this.rc("operations/cleanup", { fs });
+  }
+
+  /**
+   * Copies a directory from source remote to destination remote
+   * @param {string} srcFs a remote name string e.g. "drive:src" for the source
+   * @param {string} dstFs a remote name string e.g. "drive:dst" for the destination
+   * @returns 
+   */
+  copy(srcFs, dstFs, options = {}) {
+    return this.rc("sync/copy", { srcFs, dstFs, ...options });
+  }
+
+  /**
+   * Copies a file from source remote to destination remote
+   * @param {string} src a source path string e.g. "drive:path/to/file"
+   * @param {string} dst a destination path string e.g. "drive:path/to/file"
+   * @returns 
+   */
+  copyto(src, dst) {
+    const [, srcFs, srcRemote] = src.match(CONNECTION_STRING);
+    const [, dstFs, dstRemote] = dst.match(CONNECTION_STRING);
+    return this.rc("operations/copyfile", {
+      srcFs, srcRemote,
+      dstFs, dstRemote,
+    });
+  }
+
+  /**
+   * Copies the URL to the object
+   * @param {string|URL} url 
+   * @param {string} dst a remote path string e.g. "drive:path/to/file"
+   * @returns 
+   */
+  copyurl(url, dst, options = {}) {
+    const [, fs, remote ] = dst.match(CONNECTION_STRING);
+    return this.rc("operations/copyurl", { url, fs, remote, ...options });
+  }
+
+  /**
+   * Removes files in the path
+   * @param {string} fs a remote name string e.g. "drive:"
+   * @returns 
+   */
+  delete(fs) {
+    return this.rc("operations/delete", { fs });
+  }
+  
+  /**
+   * Removes a single file
+   * @param {string} src a remote path string e.g. "drive:path/to/file"
+   * @returns 
+   */
+  deletefile(src) {
+    const [, fs, remote ] = src.match(CONNECTION_STRING);
+    return this.rc("operations/deletefile", { fs, remote });
+  }
+
+  /**
+   * Produces a hashsum file for all the objects in the path.
+   * @param {string} fs 
+   * @returns 
+   */
+  hashsum(fs, options = {}) {
+    return this.rc("operations/hashsum", { fs, ...options });
+  }
+
+  /**
+   * Creates or retrieves a public link to the given file or folder.
+   * @param {string} target a remote path string e.g. "drive:path/to/file"
+   * @param {Object} options 
+   * @returns 
+   */
+  link(target, options = {}) {
+    const [, fs, remote ] = target.match(CONNECTION_STRING);
+    return this.rc("operations/publiclink", { fs, remote, ...options });
+  }
+
+  /**
+   * Lists the given remote and path in JSON format
+   * @param {string} src 
+   * @returns 
+   */
+  lsjson(src, options = {}) {
+    const [, fs, remote ] = src.match(CONNECTION_STRING);
+    return this.rc("operations/list", { fs, remote, opt: options });
+  }
+
+  /**
+   * Makes a destination directory
+   * @param {string} dst 
+   * @returns 
+   */
+  mkdir(dst) {
+    const [, fs, remote ] = dst.match(CONNECTION_STRING);
+    return this.rc("operations/mkdir", { fs, remote });
+  }
+
+  /**
+   * Moves a directory from source remote to destination remote
+   * @param {string} srcFs a remote name string e.g. "drive:src" for the source
+   * @param {string} dstFs a remote name string e.g. "drive:dst" for the destination
+   * @returns 
+   */
+  move(srcFs, dstFs, options = {}) {
+    return this.rc("sync/move", { srcFs, dstFs, ...options });
+  }
+
+  noop(args = {}) {
+    return this.rc("rc/noop", args);
+  }
+
+  obscure(password) {
+    return this.rc("core/obscure", { clear: password }).obscured;
+  }
+  
+  /**
+   * Removes a directory or container and all of its contents
+   * @param {string} dst
+   * @returns 
+   */
+  purge(dst) {
+    const [, fs, remote ] = dst.match(CONNECTION_STRING);
+    return this.rc("operations/purge", { fs, remote });
+  }
+
+  /**
+   * Removes an empty directory or container
+   * @param {string} dst 
+   * @returns 
+   */
+  rmdir(dst) {
+    const [, fs, remote ] = dst.match(CONNECTION_STRING);
+    return this.rc("operations/rmdir", { fs, remote });
+  }
+
+  /**
+   * Removes all the empty directories in the path
+   * @param {string} dst 
+   * @returns 
+   */
+  rmdirs(dst, options = {}) {
+    const [, fs, remote ] = dst.match(CONNECTION_STRING);
+    return this.rc("operations/rmdirs", { fs, remote, ...options });
+  }
+
+  /**
+   * Changes storage tier or class on all files in the path
+   * @param {string} fs a remote name string e.g. "drive:"
+   * @returns 
+   */
+  settier(fs) {
+    return this.rc("operations/settier", { fs });
+  }
+
+  /**
+   * Changes storage tier or class on all files in the path
+   * @param {string} dst a remote name string e.g. "drive:path/to/dir"
+   * @returns 
+   */
+  settierfile(dst) {
+    const [, fs, remote ] = dst.match(CONNECTION_STRING);
+    return this.rc("operations/settierfile", { fs, remote });
+  }
+
+  /**
+   * Count the number of bytes and files in remote
+   * @param {string} fs aa remote name string e.g. "drive:path/to/dir"
+   * @returns 
+   */
+  size(fs) {
+    return this.rc("operations/size", { fs });
+  }
+
+  /**
+   * Syncs a directory from source remote to destination remote
+   * @param {string} srcFs a remote name string e.g. "drive:src" for the source
+   * @param {string} dstFs a remote name string e.g. "drive:dst" for the destination
+   * @returns 
+   */
+  sync(srcFs, dstFs, options = {}) {
+    return this.rc("sync/sync", { srcFs, dstFs, ...options });
   }
 
   /** Remote controls rclone
