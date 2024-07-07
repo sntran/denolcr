e10099e6b48648cd04ec338d0f5fb0527dc89a6f
Sơn Trần-Nguyễn <github@sntran.com>
2024-07-06 20:32:42 -0500

    refactor(cmd/rcat): switch to `node:process/stdin`

--- cmd/rcat/main.js
+++ cmd/rcat/main.js
@@ -1,3 +1,4 @@
+import process from "node:process";
 import { fetch } from "../../mod.js";
 
 /**
@@ -7,9 +8,19 @@ import { fetch } from "../../mod.js";
  * @returns {Promise<Response>}
  */
 export function rcat(destination, flags) {
+  const body = new ReadableStream({
+    start(controller) {
+      process.stdin.on("data", (chunk) => {
+        controller.enqueue(chunk);
+      });
+      process.stdin.on("end", () => {
+        controller.close();
+      });
+    },
+  });
+
   return fetch(`${destination}?${new URLSearchParams(flags)}`, {
     method: "PUT",
-    // TODO: Switch to `node:process.stdin`.
-    body: Deno.stdin.readable,
+    body,
   });
 }
